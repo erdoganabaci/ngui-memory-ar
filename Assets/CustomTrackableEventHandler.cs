e9b2dbc0ae7d802b7c3fbefe693ca6ac34b1df16
erdoganabaci <erdoganabaci97@gmail.com>
2023-04-24 15:27:55 +0200

    add more animals and implement video display during matching qr code

--- Assets/CustomTrackableEventHandler.cs
+++ Assets/CustomTrackableEventHandler.cs
@@ -1,21 +1,47 @@
 using System;
 using UnityEngine;
 using Vuforia;
+using UnityEngine.Video;
+
 
 public class CustomTrackableEventHandler : DefaultObserverEventHandler
 {
     public GameManager gameManager;
 
-    protected override void Start()
-    {
-        base.Start();
+  protected override void Start()
+{
+    base.Start();
 
-        GameObject gameManagerObject = GameObject.Find("GameManager");
-        if (gameManagerObject != null)
+    // Find the GameManager object in the scene
+    GameObject gameManagerObject = GameObject.Find("GameManager");
+    if (gameManagerObject != null)
+    {
+        // Get the GameManager component from the object
+        gameManager = gameManagerObject.GetComponent<GameManager>();
+    }
+
+    // If the GameManager component is found, add the video clips to the dictionary
+    if (gameManager != null)
+    {
+                      Debug.Log("gameManager is not null");
+
+        // Load the video clips from the Assets/Videos folder
+        VideoClip bear_qr1_video = Resources.Load<VideoClip>("Videos/bear_qr1_video");
+        VideoClip bear_qr2_video = Resources.Load<VideoClip>("Videos/bear_qr2_video");
+
+         // Add the video clips to the qrCodeToVideoClip dictionary with unique keys
+        if (!gameManager.qrCodeToVideoClip.ContainsKey("bear_qr1"))
         {
-            gameManager = gameManagerObject.GetComponent<GameManager>();
+            gameManager.qrCodeToVideoClip.Add("bear_qr1", bear_qr1_video);
+        }
+
+        if (!gameManager.qrCodeToVideoClip.ContainsKey("bear_qr2"))
+        {
+            gameManager.qrCodeToVideoClip.Add("bear_qr2", bear_qr2_video);
         }
     }
+}
+
 
     protected override void OnTrackingFound()
     {
@@ -25,8 +51,11 @@ public class CustomTrackableEventHandler : DefaultObserverEventHandler
         if (gameManager != null)
         {
                       Debug.Log(" before OnQRCodeDetected");
+                      Debug.Log(" before OnQRCodeDetected qr name"+ mObserverBehaviour.TargetName);
 
             gameManager.OnQRCodeDetected(mObserverBehaviour.TargetName);
+            // gameManager.PlayVideoForQRCode(mObserverBehaviour.TargetName);
+
         }
     }
 }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,15 +1,58 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System;
+using UnityEngine.Video;
+
 
 public class GameManager : MonoBehaviour
 {
     public AudioSource successAudioSource;
     public AudioSource failAudioSource;
+    public UnityEngine.Video.VideoPlayer videoPlayer;
+    public VideoClip bear_qr1_video;
+    public VideoClip bear_qr2_video;
+
 
     private string lastDetectedQRCode = null;
     private int score = 0;
 
+        // private Dictionary<string, string> qrCodeToVideoURL = new Dictionary<string, string>
+    // {
+    //     { "bear_qr1", "https://drive.google.com/file/d/1fCDPJNvxnWHtwry9srx1MXiCpHRptbwT/preview" },
+    //     { "bear_qr2", "https://drive.google.com/file/d/1fCDPJNvxnWHtwry9srx1MXiCpHRptbwT/preview" },
+    //     // Add more mappings here
+    // };
+
+     [SerializeField]
+    public Dictionary<string, VideoClip> qrCodeToVideoClip = new Dictionary<string, VideoClip>
+    {
+        { "bear_qr1", null },
+        { "bear_qr2", null },
+        // Add more mappings here
+    };
     private void Start()
     {
+         // Find the "Video Plane" object
+        Transform videoPlaneTransform = transform.Find("Video Plane");
+
+        if (videoPlaneTransform == null)
+        {
+            Debug.LogError("Video Plane not found!");
+            return;
+        }
+
+        // Get the renderer component of the "Video Plane" object
+        Renderer videoPlaneRenderer = videoPlaneTransform.GetComponent<Renderer>();
+
+        if (videoPlaneRenderer == null)
+        {
+            Debug.LogError("Renderer component not found on Video Plane!");
+            return;
+        }
+
+        // Assign the video plane's renderer to the video player's targetMaterialRenderer
+        videoPlayer.targetMaterialRenderer = videoPlaneRenderer;
+
         GameObject successAudioSourceObject = GameObject.Find("Success Audio Source");
         if (successAudioSourceObject != null)
         {
@@ -52,10 +95,102 @@ public class GameManager : MonoBehaviour
         }
 
         lastDetectedQRCode = null;
+        // PlayVideo(qrCodeName);
+
     }
 
     private bool IsMatchingQRCode(string qrCode1, string qrCode2)
     {
         return qrCode1.Split('_')[0] == qrCode2.Split('_')[0];
     }
+
+
+private void PlayVideo(string qrCodeName)
+{
+    if (qrCodeToVideoClip.ContainsKey(qrCodeName))
+    {
+        VideoClip videoClip = qrCodeToVideoClip[qrCodeName];
+            Debug.Log("videoClip name: "+videoClip);
+            Debug.Log("qrCodeToVideoClip name: "+qrCodeToVideoClip);
+            Debug.Log("qrCodeName name: "+qrCodeName);
+
+        if (videoClip != null)
+        {
+            videoPlayer.clip = videoClip;
+            videoPlayer.Play();
+        }
+        else
+        {
+            // The video clip is null, so load it from the Resources folder
+            videoClip = Resources.Load<VideoClip>("Videos/" + qrCodeName + "_video");
+            if (videoClip != null)
+            {
+                qrCodeToVideoClip[qrCodeName] = videoClip;
+                videoPlayer.clip = videoClip;
+                videoPlayer.Play();
+            }
+            else
+            {
+                Debug.LogError("VideoClip not found for QR code: " + qrCodeName);
+            }
+        }
+    }
+    else
+    {
+        Debug.LogError("QR code not found in the dictionary: " + qrCodeName);
+    }
+}
+
+
+// private void PlayVideo(string qrCodeName)
+// {
+//     if (qrCodeToVideoClip.ContainsKey(qrCodeName))
+//     {
+//         VideoClip videoClip = qrCodeToVideoClip[qrCodeName];
+
+//         if (videoClip != null)
+//         {
+//             videoPlayer.clip = videoClip;
+//             videoPlayer.Play();
+//         }
+//         else
+//         {
+//             Debug.LogError("VideoClip not found for QR code: " + qrCodeName);
+//         }
+//     }
+//     else
+//     {
+//         Debug.LogError("QR code not found in the dictionary: " + qrCodeName);
+//     }
+// }
+
+//     public void PlayVideoForQRCode(string qrCode)
+// {
+//      Debug.Log("video played");
+
+//     string videoURL = GetVideoURLForQRCode(qrCode);
+//     if (!string.IsNullOrEmpty(videoURL))
+//     {
+//         videoPlayer.url = videoURL;
+//         videoPlayer.Play();
+//     }
+//     else
+//     {
+//         Debug.LogError("No video found for QR code: " + qrCode);
+//     }
+// }
+
+
+
+    // private string GetVideoURLForQRCode(string qrCode)
+    // {
+    //     if (qrCodeToVideoURL.TryGetValue(qrCode, out string videoURL))
+    //     {
+    //         return videoURL;
+    //     }
+    //     else
+    //     {
+    //         return null;
+    //     }
+    // }
 }
